8168395387d7865c501ba453e50f73830927f5b1
오수빈 Subin OH <almighty5@snu.ac.kr>
2025-07-04 06:10:39 +0900

    feat: 분석, 결과 페이지 네비게이션 구현

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -7,6 +7,12 @@ import Person from "./Person.svg?react";
 import ChemiIconFull from "./ChemiIconFull.svg?react";
 import EyeOpened from "./eyeOpened.svg";
 import EyeClosed from "./eyeClosed.svg";
+import NavChemi from "./NavChemi.svg?react";
+import NavLover from "./NavLover.svg?react";
+import NavMBTI from "./NavMBTI.svg?react";
+import NavMore from "./NavMore.svg?react";
+import NavSome from "./NavSome.svg?react";
+import NavStat from "./NavStat.svg?react";
 
-export { X, FileUpload, SearchMore, Chatto, ArrowDown, Person, ChemiIconFull, EyeClosed, EyeOpened };
+export { X, FileUpload, SearchMore, Chatto, ArrowDown, Person, ChemiIconFull, EyeClosed, EyeOpened, NavChemi, NavLover, NavMBTI, NavMore, NavSome, NavStat };
 
